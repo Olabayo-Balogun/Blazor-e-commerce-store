592e245563e0669a21a0682d775d76a59dea85e1
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-03-13 23:01:59 +0100

    Fixing cyclical code and removing the the erring code from the category class

--- Server/Services/CategoryService/CategoryService.cs
+++ Server/Services/CategoryService/CategoryService.cs
@@ -1,4 +1,6 @@
-﻿using AyacOnlineStore.Shared;
+﻿using AyacOnlineStore.Server.Data;
+using AyacOnlineStore.Shared;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,22 +10,20 @@ namespace AyacOnlineStore.Server.Services.CategoryService
 {
     public class CategoryService : ICategoryService
     {
-        public List<Category> Categories { get; set; } = new List<Category>
+        private readonly DataContext _context;
+
+        public CategoryService(DataContext context)
         {
-            new Category { Id = 1, Name = "Cereals", Url = "cereals", Icon = "cereals" },
-            new Category { Id = 2, Name = "Fruits", Url = "fruits", Icon = "fruits" },
-            new Category { Id = 3, Name = "Oils", Url = "oils", Icon = "oils" },
-            new Category { Id = 4, Name = "Poultry", Url = "poultry", Icon = "poultry" },
-            new Category { Id = 5, Name = "Vegetables", Url = "vegetables", Icon = "vegetables" }
-        };
+            _context = context;
+        }
         public async Task<List<Category>> GetCategories()
         {
-            return Categories; 
+            return await _context.Categories.ToListAsync(); 
         }
 
         public async Task<Category> GetCategoryByUrl(string categoryUrl)
         {
-            return Categories.FirstOrDefault(c => c.Url.ToLower().Equals(categoryUrl.ToLower()));
+            return await _context.Categories.FirstOrDefaultAsync(c => c.Url.ToLower().Equals(categoryUrl.ToLower()));
         }
     }
 }
