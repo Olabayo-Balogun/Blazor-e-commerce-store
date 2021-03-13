bf7a43585756ea17c9f1eb19ce04a8208b4068f5
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-03-13 15:10:59 +0100

    Created services folder and registered dependency injection for the product services

---
+++ Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AyacOnlineStore.Client.Services.CategoryService
+{
+    public class CategoryService : ICategoryService
+    {
+    }
+}
---
+++ Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AyacOnlineStore.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+
+    }
+}
