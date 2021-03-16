39dbd2b8663e361ac3ca7fba7324901f5cacd28e
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-03-16 13:04:15 +0100

    Added statistics class to enable the logging of the number of user visits

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -17,6 +17,7 @@ namespace AyacOnlineStore.Server.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<PurchaseOption> PurchaseOptions { get; set; }
+        public DbSet<Statistics> Statistics { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
