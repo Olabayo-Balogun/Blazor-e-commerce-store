da846a8ca6aa5a8a78e2a54d8f36ab6ef20df428
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-03-16 13:05:03 +0100

    Added statistics class migration

---
+++ Server/Migrations/20210316120432_Statistics.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AyacOnlineStore.Server.Migrations
+{
+    public partial class Statistics : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Statistics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Visits = table.Column<int>(type: "int", nullable: false),
+                    LastVisit = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statistics", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Statistics");
+        }
+    }
+}
